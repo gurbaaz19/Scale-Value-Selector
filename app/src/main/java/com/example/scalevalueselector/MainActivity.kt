c1b2165be53dd1d5074b559ab7ced340934efbd9
Gurbaaz Singh Gill <gurbaaz19@gmail.com>
2022-07-26 19:45:41 +0530

    start

--- app/src/main/java/com/example/scalevalueselector/MainActivity.kt
+++ app/src/main/java/com/example/scalevalueselector/MainActivity.kt
@@ -1,16 +1,17 @@
 package com.example.scalevalueselector
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.TextView
-import com.kevalpatel2106.rulerpicker.*
+import androidx.appcompat.app.AppCompatActivity
+import com.kevalpatel2106.rulerpicker.RulerValuePicker
+import com.kevalpatel2106.rulerpicker.RulerValuePickerListener
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        var rulerValuePicker:RulerValuePicker=findViewById(R.id.ruler_picker)
+        var rulerValuePicker: RulerValuePicker = findViewById(R.id.ruler_picker)
 
         var textView: TextView = findViewById(R.id.textView)
 
@@ -20,13 +21,25 @@ class MainActivity : AppCompatActivity() {
             override fun onValueChange(selectedValue: Int) {
                 //Value changed and the user stopped scrolling the ruler.
                 //Application can consider this value as final selected value.
-                textView.text = selectedValue.toString()
+                if (selectedValue > 200) {
+                    textView.text = "200"
+                    rulerValuePicker.selectValue(200)
+
+                } else {
+                    textView.text = selectedValue.toString()
+                }
             }
 
             override fun onIntermediateValueChange(selectedValue: Int) {
                 //Value changed but the user is still scrolling the ruler.
                 //This value is not final value. You can utilize this value to display the current selected value.
-                textView.text=selectedValue.toString()
+                if (selectedValue > 200) {
+                    textView.text = "200"
+                    rulerValuePicker.selectValue(200)
+
+                } else {
+                    textView.text = selectedValue.toString()
+                }
             }
         })
     }
